5d05f8d018a44472d7fe77206452d4e81a95d0b1
sri <sri.sk222@gmail.com>
2020-07-14 15:57:55 +0530

    Updated the URLS and requiremets

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 
 urlpatterns = [
     url(r'^api/patient/findPatient/(?P<pk>[0-9]+)/$', views.find_patient, name='Find_Patient'),
-    url(r'^`1       ``st/$', views.patient_list, name='Get_ALL_PatientList'),
+    url(r'^api/patient/patientlist/$', views.patient_list, name='Get_ALL_PatientList'),
     url(r'^api/patient/register/$', views.register, name='Register_Patient'),
     url(r'^api/patient/getPatient/', views.getPatientDetails, name = 'Get_Patient_details'),
     url(r'^api/patient/login/$', views.login, name='Patient_login')
